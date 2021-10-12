58b567a93e16ea9c8816d8e3de1e6a5d693e86f4
KITO <kiyj585@gmail.com>
2021-10-13 00:19:33 +0300

    variant 2 (random color)

--- solid_test_app/lib/main.dart
+++ solid_test_app/lib/main.dart
@@ -85,3 +85,78 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
+
+// Variant 2 for filling the background with a random color
+
+// class MyPage extends StatefulWidget {
+//   const MyPage({Key? key, required this.title}) : super(key: key);
+
+//   final String title;
+
+//   @override
+//   State<MyPage> createState() => _MyPageState();
+// }
+
+// class _MyPageState extends State<MyPage> {
+//   final Random _random = Random();
+//   String _text = 'Hey there';
+//   Color _color = const Color(0x00ffffff);
+
+//   void initState() {
+//     setState(() {
+//       generateColor();
+//       super.initState();
+//     });
+//   }
+
+//   void generateColor() {
+//     setState(() {
+//       _color = Color.fromRGBO(
+//       _random.nextInt(256),
+//       _random.nextInt(256),
+//       _random.nextInt(256),
+//       1.0,
+//       );
+//     });
+//   }
+
+//   void displayColorCode() {
+//     setState(() {
+//       _text =
+//           'Background Color (RGB): (${_color.red}, ${_color.green}, ${_color.blue})';
+//     });
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         leading: Builder(
+//           builder: (BuildContext context) {
+//             return IconButton(
+//               icon: const FlutterLogo(),
+//               onPressed: () {
+//                 generateColor();
+//                 displayColorCode();
+//               },
+//             );
+//           },
+//         ),
+//         title: Text(widget.title),
+//         backgroundColor: _color,
+//       ),
+//       body: GestureDetector(
+//         onTap: () {
+//           generateColor();
+//           displayColorCode();
+//         },
+//         child: Container(
+//           color: _color,
+//           alignment: Alignment.center,
+//           child: Text(_text),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
