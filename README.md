2ae9ed8e70c3529fdcc7bcbaf90298056dd823a6
KITO <kiyj585@gmail.com>
2021-10-12 19:43:35 +0300

    test assignment implementation

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Solid-Test-App
+
+## Test assignment:
+
+* Install Flutter
+* Create a test application
+* The application should: display the text "Hey there" in the middle of the screen and after tapping anywhere on the screen, a background color should be changed to a randomly generated color (should be able to generate 16777216 colors using RGB). You can also add any other feature to the app - that adds bonus points
+* Please do not use any external libraries for color generation
+* Push code to a GitHub
+* Send the link to a GitHub repository back
+
+![Main screen](/images/mainscreen.jpg)
+
+![After tapping](/images/aftertapping.jpg)
---
+++ solid_test_app/lib/main.dart
@@ -0,0 +1,88 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Solid Test App',
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(title: 'Solid Test App'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String _text = 'Hey there';
+  Color _color = const Color(0x00ffffff);
+
+  void initState() {
+    setState(() {
+      generateColor();
+      displayColorCode();
+      super.initState();
+    });
+  }
+
+  void generateColor() {
+    setState(() {
+      _color =
+          Color((Random().nextDouble() * 0xFFFFFF).toInt()).withOpacity(1.0);
+    });
+  }
+
+  void displayColorCode() {
+    setState(() {
+      _text =
+          'Background Color (RGB): (${_color.red}, ${_color.green}, ${_color.blue})';
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: Builder(
+          builder: (BuildContext context) {
+            return IconButton(
+              icon: const FlutterLogo(),
+              onPressed: () {
+                generateColor();
+                displayColorCode();
+              },
+            );
+          },
+        ),
+        title: Text(widget.title),
+        backgroundColor: _color,
+      ),
+      body: GestureDetector(
+        onTap: () {
+          generateColor();
+          displayColorCode();
+        },
+        child: Container(
+          color: _color,
+          alignment: Alignment.center,
+          child: Text(_text),
+        ),
+      ),
+    );
+  }
+}
